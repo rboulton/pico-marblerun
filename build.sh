0b8af6ebf6371c01627c204cfa83eab3cb93e8f4
Richard Boulton <richard@tartarus.org>
2024-05-16 22:06:56 +0100

    Add pimoroni libraries

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-# Checkout the pico-sdk in a peer directory to this project.
-# Make sure it submodules are initted
+
+if [ ! -e pico-sdk ]; then
+  git clone https://github.com/raspberrypi/pico-sdk
+  cd pico-sdk
+  git submodule update --init
+  cd ..
+fi
+export PICO_SDK_PATH=`pwd`/pico-sdk
+
+if [ ! -e pimoroni-pico ]; then
+  git clone https://github.com/pimoroni/pimoroni-pico
+  cd pimoroni-pico
+  git submodule update --init
+  cd ..
+fi
+export PIMORONI_PICO_PATH=`pwd`/pico-sdk
 
 mkdir build
 cd build
-cmake .. -DPICO_SDK_PATH=../../pico-sdk -DPICO_BOARD=pimoroni_plasma2040
+cmake .. -DPICO_BOARD=pimoroni_plasma2040
+make
